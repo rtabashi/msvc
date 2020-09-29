ad88160d3874b88b6d92544d7d1c7be5cd2debf9
rtabashi <ryoheitabashi@gmail.com>
2020-09-29 23:12:12 +0900

    modified rc4

--- rc4/rc4.c
+++ rc4/rc4.c
@@ -18,9 +18,9 @@ void swap(unsigned char* c1, unsigned char* c2)
 
 void init_rc4(unsigned char* table, unsigned char* key, size_t key_size)
 {
-	int				i;
-	unsigned char	index1 = 0;
-	unsigned char	index2 = 0;
+	int i;
+	unsigned char index1 = 0;
+	unsigned char index2 = 0;
 
 	for (i = 0; i < 0x100; i++) {
 		table[i] = i;
@@ -37,10 +37,10 @@ void init_rc4(unsigned char* table, unsigned char* key, size_t key_size)
 
 void rc4(unsigned char* table, unsigned char** dst_data, unsigned char* src_data, size_t data_size)
 {
-	unsigned char*	data;
-	unsigned int	i;
-	unsigned char	x = 0;
-	unsigned char	y = 0;
+	unsigned char* data;
+	unsigned int i;
+	unsigned char x = 0;
+	unsigned char y = 0;
 
 	data = (unsigned char*)malloc(data_size + 1);
 	memset(data, 0, data_size + 1);
@@ -56,9 +56,9 @@ void rc4(unsigned char* table, unsigned char** dst_data, unsigned char* src_data
 
 void do_rc4(unsigned char** dst_data, unsigned char* key, unsigned char* src_data)
 {
-	unsigned char	table[0x100];
-	size_t			key_size = strlen(key);
-	size_t			data_size = strlen(src_data);
+	unsigned char table[0x100];
+	size_t key_size = strlen(key);
+	size_t data_size = strlen(src_data);
 
 	init_rc4(table, key, key_size);
 	rc4(table, dst_data, src_data, data_size);
@@ -76,11 +76,11 @@ unsigned char char_to_hex(unsigned char c)
 
 unsigned char* ascii_to_bin(unsigned char* ascii)
 {
-	unsigned char*	bin;
-	unsigned char	h_hex = 0;
-	unsigned char	l_hex = 0;
-	size_t			len;
-	int				i;
+	unsigned char* bin;
+	unsigned char h_hex = 0;
+	unsigned char l_hex = 0;
+	size_t len;
+	int i;
 
 	len = strlen(ascii) / 2;
 	bin = (unsigned char*)malloc(len + 1);
@@ -96,9 +96,9 @@ unsigned char* ascii_to_bin(unsigned char* ascii)
 
 int main(int argc, char* argv[])
 {
-	unsigned char*	key;
-	unsigned char*	src_data;
-	unsigned char*	dst_data;
+	unsigned char* key;
+	unsigned char* src_data;
+	unsigned char* dst_data;
 
 	if (argc != 3) {
 		usage(argv[0]);
